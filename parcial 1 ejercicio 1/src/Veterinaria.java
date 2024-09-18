a287d4048e991ec23672c04810fae476f91e9073
Florb11 <florbergman11@gmail.com>
2024-09-18 13:41:37 -0300

    primer commit

---
+++ parcial 1 ejercicio 1/src/Veterinaria.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+
+public class Veterinaria {
+    //Atributos
+    private String nombre;
+    private int edad;
+    private double peso;
+    private String dueno;
+    private String veterinario;
+    private LocalDate fechaTurno;
+    private boolean tratamiento;
+    private boolean estadoDeSalud;
+
+
+    //Constructor
+
+    public Veterinaria (String nombre, int edad,double peso,String dueno,String veterinario,LocalDate fechaTurno,boolean tratamiento,boolean estadoDeSalud){
+
+
+
+    }
+
+
+
+}
