e6f0ad3b71529d91ae24d1bde72478b941c299a0
Florb11 <florbergman11@gmail.com>
2024-09-18 16:56:55 -0300

    probando

--- parcial 1 ejercicio 1/src/Main.java
+++ parcial 1 ejercicio 1/src/Main.java
@@ -1,5 +1,43 @@
+import javax.swing.*;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        String nombre = JOptionPane.showInputDialog("Ingrese el nombre del animal:");
+        String dueno = JOptionPane.showInputDialog("Ingrese el nombre del dueño:");
+        int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad del animal:"));
+        double peso = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el peso del animal:"));
+        String veterinario = JOptionPane.showInputDialog("Ingrese el nombre del veterinario:");
+        Veterinaria veterinaria1 = new Veterinaria(nombre, edad, peso, dueno, veterinario, null, false, false);
+
+        String[] menu = {"Ver estado del animal", "Realizar evaluacion", "Programar turno", "Mejorar salud", "Verificar turno", "Salir" };
+        int opcion = 0;
+
+        do {
+            opcion = JOptionPane.showOptionDialog(null, "Elija una opción", "Menu Veterinaria", JOptionPane.DEFAULT_OPTION, 0, null, menu, menu[0]);
+
+            switch (opcion) {
+                case 0:
+                    JOptionPane.showMessageDialog(null, veterinaria1);
+                    break;
+                case 1:
+                    veterinaria1.realizarEvaluacion();
+                    break;
+                case 2:
+                    veterinaria1.programarTurno();
+                    break;
+                case 3:
+                    veterinaria1.aplicarTratamiento();
+                    break;
+                case 4:
+                    veterinaria1.verificarTurno();
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, "Gracias por utilizar el sistema.", "", JOptionPane.DEFAULT_OPTION);
+                    break;
+            }
+        } while (opcion != 5);
+    }
+
 
     }
-}
--- parcial 1 ejercicio 1/src/Veterinaria.java
+++ parcial 1 ejercicio 1/src/Veterinaria.java
@@ -1,4 +1,6 @@
+import javax.swing.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Veterinaria {
     //Atributos
@@ -101,17 +103,129 @@ public class Veterinaria {
 
     //metodos
 
-    // metodo para el estado de salud
 
-    public String mostrarEstado (boolean estado){
-        if(estado){
-            return "No necesita tratamiento";
+    public void realizarEvaluacion(){
+        JOptionPane.showMessageDialog(null,"Comenzando la evaluacion");
+
+        if(this.estadoDeSalud){
+            JOptionPane.showMessageDialog(null,"El animal esta en buen estado de salud y no requiere tratamiento");
+            this.tratamiento=false;
+        }else{
+            JOptionPane.showMessageDialog(null,"El animal necesita tratamiento");
+            this.tratamiento=true;
+
         }
-        return "El animal necesita tratamiento";
+        int respuesta = JOptionPane.showConfirmDialog(null, "¿Queres agendar un turno?", "Programar Turno", JOptionPane.YES_NO_OPTION);
+
+        if (respuesta == JOptionPane.YES_OPTION) {
+            programarTurno();
+        }
+
+    }
+
+    public void aplicarTratamiento(){
+        if(this.tratamiento){
+            this.estadoDeSalud=true;
+            this.tratamiento=false;
+            JOptionPane.showMessageDialog(null,"El tratamiento fue efectivo el animal ya no necesita tratamiento");
+        }else{
+            JOptionPane.showMessageDialog(null,"el animal esta bien");
+        }
+    }
+    public void programarTurno(){
+        String[] opciones = {"Agendar de aca a 1 semana", "Agendar fecha personalizada", "Cancelar"};
+        int opcion = JOptionPane.showOptionDialog(null, "¿Para cuando queres agendar el turno?", "Agendar Turno",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                opciones,
+                opciones[0]);
+
+        switch (opcion) {
+            case 0:
+                this.fechaTurno = LocalDate.now().plusWeeks(1);
+                JOptionPane.showMessageDialog(null, "El turno esta programado para: " + this.fechaTurno);
+                break;
+            case 1:
+                boolean fechaValida = false;
+                int anio = 0, mes = 0, dia = 0;
+                do {
+                    anio = validarNumeros("Ingrese año:");
+                    mes = validarNumeros("Ingrese mes:");
+                    dia = validarNumeros("Ingrese día:");
+                    if (validarFecha(anio, mes, dia)) {
+                        this.fechaTurno = LocalDate.of(anio, mes, dia);
+                        JOptionPane.showMessageDialog(null, "El turno esta programado para: " + this.fechaTurno);
+                        fechaValida = true;
+                    } else {
+                        JOptionPane.showMessageDialog(null, "La fecha debe ser posterior a la fecha actual");
+                    }
+                } while (!fechaValida);
+                break;
+
+            case 2:
+                JOptionPane.showMessageDialog(null, "No se agendo ningun turno.");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opcion no valida.");
+                break;
+        }
+    }
+
+    public void verificarTurno(){
+        if(LocalDate.now().isAfter(this.fechaTurno)){
+            JOptionPane.showMessageDialog(null,"La fecha del turno ya paso, necesita repogramar");
+        }else{
+            Period tiempoRestante = Period.between(LocalDate.now(),this.fechaTurno);
+            JOptionPane.showMessageDialog(null,"Faltan: "+tiempoRestante.getDays()+" dias para el turno");
+        }
+    }
+    public String validarCaracteres(String mensaej) {
+        String palabra = "";
+        while (palabra.equals("")) {
+            palabra = JOptionPane.showInputDialog(mensaej);
+        }
+        return palabra;
+    }
+    public int validarNumeros(String mensaje) {
+        boolean flag ;
+        String num ="";
+        do {
+            flag =true;
+            num = JOptionPane.showInputDialog(mensaje);
+            while (num.isEmpty()) {
+                num = JOptionPane.showInputDialog(mensaje);
+            }
+            for (int i = 0; i < num.length(); i++) {
+                if (!Character.isDigit(num.charAt(i))) {
+                    flag = false;
+                    break;
+                }
+            }
+        } while (!flag);
+
+        return Integer.parseInt(num);
+    }
+    public boolean validarFecha (int anio,int mes, int dia){
+        LocalDate fechaIngresada = LocalDate.of(anio, mes, dia);
+        return fechaIngresada.isAfter(LocalDate.now());
+
     }
 
 
 
 
-
+    @Override
+    public String toString() {
+        return "Veterinaria{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", peso=" + peso +
+                ", dueno='" + dueno + '\'' +
+                ", veterinario='" + veterinario + '\'' +
+                ", fechaTurno=" + fechaTurno +
+                ", tratamiento=" + tratamiento +
+                ", estadoDeSalud=" + estadoDeSalud +
+                '}';
+    }
 }
